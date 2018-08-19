2b4ed01f369c5acc192ecca7921bb6495e9544c7
immgp9 <mandar.p17151@iimtrichy.ac.in>
2018-08-19 19:52:09 +0530

    19:52

--- file1.R
+++ file1.R
@@ -4,3 +4,11 @@ git config --global user.email "mandar.p17151@iimtrichy.ac.in"
 git config --global user.name "immgp9"
 
 women
+
+plot(women)
+install.packages('car')
+?women
+x= 1:100
+x
+y= seq(1,100, by=2)
+y
